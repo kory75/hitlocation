c245aec506501ecb765b3c0a1b725a0260e242e4
Kory <kory75@gmail.com>
2020-04-29 01:12:29 +0100

    Basic layout and hit location calculation
    Basic d23 used to select hit location.
    Add basic layout.

--- src/app/hit-location/hit-location.component.ts
+++ src/app/hit-location/hit-location.component.ts
@@ -9,7 +9,21 @@ export class HitLocationComponent implements OnInit {
 
   constructor() { }
 
+  public result: string ='';
+
   ngOnInit(): void {
+  }
+
+  roll() {
+    const diceRoll = Math.floor(Math.random() * 22);
+    this.result = [
+      'skull cap', 'face',  
+      'left shoulder', 'left upper arm', 'left lower arm', 'left hand',
+      'right shoulder', 'right upper arm', 'right lower arm', 'right hand',
+      'chest', 'shoulder blade', 'stomach', 'spine', 'pelvis',
+      'left thighs', 'left knee', 'left calf', 'left foot',
+      'right thighs', 'right knee', 'right calf', 'right foot',
+    ][diceRoll]
   }
 
 }
